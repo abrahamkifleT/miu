e8d871a8052d375ac2d480481089af3e01b0120c
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-11-19 16:08:00 -0800

    nextPerfectSquare that return the first square that is greater than its integer argument

--- NextPerfectSquare.java
+++ NextPerfectSquare.java
@@ -1,3 +1,21 @@
 public class NextPerfectSquare {
-    
+
+    public static int nextPerfectSquare(int n){
+        if (n < 0 ){
+            return 0;
+        }
+        for (int i = 1; ; i++){
+            int perfectNumber = i * i;
+            if (perfectNumber > n){
+                return perfectNumber;
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        int nextPerfectNumber = nextPerfectSquare(-5);
+        System.out.println(nextPerfectNumber);
+    }
 }
