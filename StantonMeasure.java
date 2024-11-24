7199440c48a18915493cbc05963ab6a4406afd8d
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-11-23 19:45:14 -0800

    StantonMeasure

---
+++ StantonMeasure.java
@@ -0,0 +1,26 @@
+public class StantonMeasure {
+
+    public static int stantonMeasure(int[] a){
+        int stantonSum = 0;
+        int allStantonCount = 0;
+        for (int i : a){
+            if (i == 1){
+                stantonSum += i;
+            }
+        }
+        for (int i : a){
+            if (i == stantonSum){
+                allStantonCount++;
+            }
+        }
+        return allStantonCount;
+    }
+
+
+
+    public static void main(String[] args){
+        int [] a = {3,1,1,4};
+        int isStanton = stantonMeasure(a);
+        System.out.println(isStanton);
+    }
+}
